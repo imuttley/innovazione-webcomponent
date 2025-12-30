4d56173903f3ab3747ff9d4a212f239332ad7394
imuttley <tommaso.nicosia@enea.it>
2025-12-30 14:50:17 +0100

    modified:   src/frontend.tsx

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -61,18 +61,19 @@ const eventLoggerComponent = r2wc(EventLogger, React, ReactDOM as any, {
 customElements.define("innovazione-event-logger", eventLoggerComponent);
 /* end of logger */
 
-const elem = document.getElementById("root")!;
+const elem = document.getElementById("root") || null;
 const app = (
   <StrictMode>
     <App />
   </StrictMode>
 );
-
-if (import.meta.hot) {
-  // With hot module reloading, `import.meta.hot.data` is persisted.
-  const root = (import.meta.hot.data.root ??= createRoot(elem));
-  root.render(app);
-} else {
-  // The hot module reloading API is not available in production.
-  createRoot(elem).render(app);
+if (elem !== null) {
+  if (import.meta.hot) {
+    // With hot module reloading, `import.meta.hot.data` is persisted.
+    const root = (import.meta.hot.data.root ??= createRoot(elem));
+    root.render(app);
+  } else {
+    // The hot module reloading API is not available in production.
+    createRoot(elem).render(app);
+  }
 }

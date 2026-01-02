cf6095c6d9fb58f10b1733103e5b37f87512e2bc
imuttley <tommaso.nicosia@enea.it>
2026-01-02 18:50:35 +0100

    renamed:    dist/index-ksw6q7q2.js -> dist/index-482wddgf.js 	renamed:    dist/index-ksw6q7q2.js.map -> dist/index-482wddgf.js.map 	modified:   dist/index.html 	modified:   dist/innovazione-webcomp.js 	modified:   src/component/DownloadPDFButton.tsx 	modified:   src/index.html

--- src/component/DownloadPDFButton.tsx
+++ src/component/DownloadPDFButton.tsx
@@ -24,40 +24,44 @@ export const DownloadPDFButton: React.FC<Printprops> = ({
     const lang: "en" | "it" = langAttribute === 'it' ? 'it' : 'en';
     const dict = lang === 'it' ? dictit : dicten;
 
-    const dwlpdf = async () => {
-
-        if (id !== undefined) {
-            const askform = async () => {
-                const resp = await fetch(`${BASE_URL}/v1/publiccard/${id}`);
-                if (resp.status === 200) {
-                    const form = await resp.json() as formTTEC;
-                    setFormdata(form);
-                } else {
-                    console.log("error fetching form data for cardnumber " + id + " status " + resp.status);
-                }
+    const getAndDownload = async () => {
+        if (formdata) {
+            const form = await (await fetch(`${BASE_URL}/v1/record/${formdata.scheda_num}`)).json() as SchedaData;
+            const slug = lang === 'it' ? formdata.slug_it : formdata.slug_en;
+            const pdfBlob = await form2pdf(form, lang, dict, `${BASE_URL}/scheda/${lang}/${slug}`);
+            if (!pdfBlob) {
+                console.log("error creating pdf");
+                return;
             }
-            await askform();
+            const url = URL.createObjectURL(pdfBlob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `${slug}.pdf`;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         }
-        if (!formdata) {
-            return;
-        }
+    }
 
-        const form = await (await fetch(`${BASE_URL}/v1/record/${formdata.scheda_num}`)).json() as SchedaData;
-        const slug = lang === 'it' ? formdata.slug_it : formdata.slug_en;
-        const pdfBlob = await form2pdf(form, lang, dict, `${BASE_URL}/scheda/${lang}/${slug}`);
-        if (!pdfBlob) {
-            console.log("error creating pdf");
-            return;
+    const askform = async () => {
+        const resp = await fetch(`${BASE_URL}/v1/publiccard/${id}`);
+        if (resp.status === 200) {
+            const form = await resp.json() as formTTEC;
+            setFormdata(form);
+        } else {
+            console.log("error fetching form data for cardnumber " + id + " status " + resp.status);
         }
-        const url = URL.createObjectURL(pdfBlob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = `${slug}.pdf`;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+    }
+
+    useEffect(() => {
+        getAndDownload();
+    }, [formdata]);
+
+    const dwlpdf = () => {
+        if (id !== undefined) {
+            askform();
+        }
     };
-
 
     const label = lang === 'it' ? 'Scarica PDF' : 'Download PDF';
     const title = lang === 'it' ? 'stampa' : 'print';
